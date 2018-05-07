d733a3df63d66e73738cc5e0b3026103a24dd625
llr392425595 <392425595@qq.com>
2018-05-07 14:59:22 +0800

    test env

--- src/main/java/com/task19/jxmall/controller/HelloController.java
+++ src/main/java/com/task19/jxmall/controller/HelloController.java
@@ -12,7 +12,6 @@ public class HelloController {
 
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public ResponseEntity<String> string() {
-
     return new ResponseEntity<>("hello", HttpStatus.OK);
   }
 
