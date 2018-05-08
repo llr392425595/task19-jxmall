732b0826efba044a5ca3d1d9fb2579a6efdf9a36
llr392425595 <392425595@qq.com>
2018-05-08 14:29:33 +0800

    update repository

--- src/main/java/com/task19/jxmall/respository/ProductRepository.java
+++ src/main/java/com/task19/jxmall/respository/ProductRepository.java
@@ -1,5 +1,20 @@
 package com.task19.jxmall.respository;
 
-public class ProductRespository {
+import com.task19.jxmall.entity.Product;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  List<Product> findAll();
+
+  Product findById(int id);
+
+  Product save(Product product);
+
+  List<Product> findByDescriptionContaining(String desc);
+
+  List<Product> findByDescriptionContainingAndName(String desc, String name);
+
+  List<Product> findByName(String name);
 }
