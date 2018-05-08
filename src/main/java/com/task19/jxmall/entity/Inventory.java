5c66b840090176a7fbb829681ad49fd8f097c433
llr392425595 <392425595@qq.com>
2018-05-08 11:29:55 +0800

    增加OneToOne的映射

--- src/main/java/com/task19/jxmall/entity/Inventory.java
+++ src/main/java/com/task19/jxmall/entity/Inventory.java
@@ -1,13 +1,17 @@
 package com.task19.jxmall.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,5 +26,10 @@ public class Inventory {
 
   @NotNull
   private int lockedCount;
+
+  @OneToOne(mappedBy = "inventory")
+  @JsonIgnoreProperties("inventory")
+  private Product product;
+
 
 }
