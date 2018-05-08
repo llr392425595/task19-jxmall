25c149328ad5a35f1a28b29672be8d2d6f746c61
llr392425595 <392425595@qq.com>
2018-05-08 09:43:26 +0800

    add entity inventory

--- src/main/java/com/task19/jxmall/entity/Inventory.java
+++ src/main/java/com/task19/jxmall/entity/Inventory.java
@@ -1,5 +1,26 @@
 package com.task19.jxmall.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Inventory {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @NotNull
+  private int count;
+
+  @NotNull
+  private int lockedCount;
 
 }
